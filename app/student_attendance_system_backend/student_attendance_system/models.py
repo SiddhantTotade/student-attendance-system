78af9f90ffda85535539b07e050208ffc04102cd
Siddhant Totade <siddhanttotade.1994@gmail.com>
2023-01-04 20:55:36 +0530

    modified : models of student

--- app/student_attendance_system_backend/student_attendance_system/models.py
+++ app/student_attendance_system_backend/student_attendance_system/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Students(models.Model):
+class Student(models.Model):
     student_name = models.CharField(max_length=50,null=True,blank=True)
     student_id = models.CharField(max_length=15,null=True,blank=True)
 
@@ -10,6 +10,6 @@ class Students(models.Model):
         return self.student_name
 
 class Attendance(models.Model):
-    student = models.ForeignKey(Students,default=None,on_delete=models.CASCADE)
+    student = models.ForeignKey(Student,default=None,on_delete=models.CASCADE)
     time_checkin = models.TimeField(auto_now=False,auto_now_add=False)
     time_checkout = models.TimeField(auto_now=False,auto_now_add=False)
