6b08ae2ecc9ee951c7cd515c747dee2faf4742c2
Siddhant Totade <siddhanttotade.1994@gmail.com>
2023-01-04 20:09:49 +0530

    created : models

--- app/student_attendance_system_backend/student_attendance_system/models.py
+++ app/student_attendance_system_backend/student_attendance_system/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Students(models.Model):
+    student_name = models.CharField(max_length=50,null=True,blank=True)
+    student_id = models.CharField(max_length=15,null=True,blank=True)
+
+class Attendance(models.Model):
+    time_checkin = models.TimeField()
+    time_checkout = models.TimeField()
