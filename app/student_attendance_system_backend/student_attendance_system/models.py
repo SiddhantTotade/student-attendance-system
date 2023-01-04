64acd34115cfd62c22f93821069be4c415030205
Siddhant Totade <siddhanttotade.1994@gmail.com>
2023-01-04 20:28:51 +0530

    installed : django restframework

--- app/student_attendance_system_backend/student_attendance_system/models.py
+++ app/student_attendance_system_backend/student_attendance_system/models.py
@@ -5,6 +5,10 @@ class Students(models.Model):
     student_name = models.CharField(max_length=50,null=True,blank=True)
     student_id = models.CharField(max_length=15,null=True,blank=True)
 
+    def __str__(self):
+        return self.student_name
+
 class Attendance(models.Model):
-    time_checkin = models.TimeField()
-    time_checkout = models.TimeField()
+    student = models.ForeignKey(Students,default=None,on_delete=models.CASCADE)
+    time_checkin = models.TimeField(auto_now=False,auto_now_add=False)
+    time_checkout = models.TimeField(auto_now=False,auto_now_add=False)
