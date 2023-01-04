6c6e88615e91042199570e307ecae9ebaacbf4f2
Siddhant Totade <siddhanttotade.1994@gmail.com>
2023-01-04 22:32:04 +0530

    created : new apis for student

--- app/student_attendance_system_backend/student_attendance_system_backend/settings.py
+++ app/student_attendance_system_backend/student_attendance_system_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -53,6 +54,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'student_attendance_system_backend.urls'
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
@@ -119,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = "/certificate_data/"
+MEDIA_ROOT = os.path.join(BASE_DIR,"certificate_data/")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
